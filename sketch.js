19590931efc0d024de67b3570b2373d4e85b98b4
godingmachine24 <90534072+godingmachine24@users.noreply.github.com>
2022-12-04 16:44:59 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,33 +1,54 @@
-var box;
+var box,box1;
 function setup() {
   createCanvas(800,800);
   box = createSprite(400,400,30,30);
   box.shapeColor = 'red';
+
+  box1 = createSprite(400,238,25,14);
+  box1.shapeColor = 'blue';
 }
 
 function draw() 
 {
-  background('gold');
-
+background('gold');
   if(keyIsDown(RIGHT_ARROW)){
-    box.position.x = box.position.x + 5;
+    box1.position.x = box1.position.x + 5;
   }
 
   if(keyIsDown(LEFT_ARROW)){
-    box.position.x = box.position.x - 5;
+    box1.position.x = box1.position.x - 5;
   }
 
   if(keyIsDown(DOWN_ARROW)){
-    box.position.y = box.position.y + 5;
+    box1.position.y = box1.position.y + 5;
   }
 
   if(keyIsDown(UP_ARROW)){
-    box.position.y = box.position.y - 5;
+    box1.position.y = box1.position.y - 5;
   }
+  
+  if(keyIsDown(RIGHT_ARROW)){
+    box1.position.x = box1.position.x + 5;
+  }
+
+  if(keyIsDown(LEFT_ARROW)){
+    box1.position.x = box1.position.x - 5;
+  }
+
+  if(keyIsDown(DOWN_ARROW)){
+    box1.position.y = box1.position.y + 5;
+  }
+
+  if(keyIsDown(UP_ARROW)){
+    box1.position.y = box1.position.y - 5;
+  }
+  
 
   drawSprites();
 
 }
+
+
 
 
 
